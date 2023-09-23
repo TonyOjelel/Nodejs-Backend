4c7f662560b6d212c99331d430f6ad01aa40352a
Tony Ojelel <tonyojelel@gmail.com>
2023-09-23 21:40:53 +0300

    1st commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Nodejs-Backend
-Simple Nodejs Backend
+
+run the following command in the terminal.
+
+# npm express mongoose passport passport-local bcryptjs cors express-session body-parser dotenv
+
+This will install Express (web framework), MongoDB (mongoose a MongoDB object modeling tool designed to work in an asynchronous environment.), Passport (authentication middleware for Node.js.), bcryptjs (for password), CORS (Cross-Origin Resource Sharing), and body-parser (for parsing incoming request bodies.), dotenv (to load environment from a .env file)
+
+
+For the environment variables (`.env file), you might have something like:
+
+PORT=5000
+MONGODB_URI=mongodb://localhost:27017/myDatabase```
+Remember to replace "myDatabase" with your actual database name.
+
+This is a basic version of the backend code, and you will need to add more layers such as error handling, validation, or security. You'll also need to implement your own passport strategy according to your needs.
+Remember to test your endpoints with a REST client like Postman oromnia to ensure they're working as expected.
---
+++ server.js
@@ -0,0 +1,81 @@
+// Import libraries
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const session = require('express-session');
+const bcrypt = require('bcryptjs');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// App setup
+const app express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(session({ secret: 'secretkey', resave: true, saveUninitialized: true }));
+app.use(passport());
+app.use(passport.session());
+
+// MongoDB setup
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console('MongoDB connected successfully'))
+.catch(err => console.log(err));
+
+// Model
+const User = mongoose.model('User', new mongoose.Schema({
+  username String,
+  password: String
+}));
+
+// Passport setup
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserialize());
+
+// Routes
+// Authentication
+app.post('/register', async (req, res) => {
+  User.register(new User username: req.body.username }), req.body.password, (err, user) => {
+    if(err) {
+      res.status(500).send(err);
+    }
+    passport('local')(req, res, () => {
+      res.send(user);
+    });
+  });
+});
+
+app.post('/login', passport.authenticate('local'), (req, res) => {
+	res.send(req.user});
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.send('Logged out');
+});
+
+// CRUD operations for User
+app.get('/users', async (req, res) {
+  const users = await User.find({});
+  res.send(users);
+});
+
+app.post('/users', async (req, res) => {
+  const newUser = await new User(req.body).save();
+ .send(newUser);
+});
+
+app.put('/users/:id', async (req, res) => {
+  const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body);
+ .send(updatedUser);
+});
+
+app.delete('/users/:id', async (req, res) => {
+  const deletedUser = await User.findByIdAndRemove(req.params.id);
+  res.send(deletedUser});
+
+// Server setup
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening on port ${process.env.PORT}`);
+});
